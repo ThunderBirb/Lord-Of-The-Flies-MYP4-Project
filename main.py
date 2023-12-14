a53cbf8793b1b160df4ddc9fc72066623f1d6b87
ThunderBirb <135807161+ThunderBirb@users.noreply.github.com>
2023-12-14 13:00:36 +0000

    Finalised character classes Added a ShowPersonInfo function displaying all stats of a person Added items and assigned values Added Error checking to Choice() function (Also changed the name back to the previous) Started working on main game loop

--- main.py
+++ main.py
@@ -1,72 +1,130 @@
 import time
 import sys
 
+PeopleList = ["piggy", "ralph", "jack", "roger", "samneric"]
+
 ValueTable = {
-    "A": 1,
-    "B": 2,
-    "C": 3
+    "Spear":2,
+    "Face paint":2,
+    "Glasses":3,
+    "Boulder":4,
+    "Castle rock":5
 }
 
+ItemTable =  {
+    "Spear":"Increased chance to aggravate",
+    "Face paint":"Lower chance to be recognized",
+    "Glasses":"Bargaining tool",
+    "Boulder":"Deadly weapon, be cautious",
+    "Castle rock":"Height advantage for user(s)"
+}
 
 class Piggy:
+    name = "Piggy"
     strengths = ["Has his glasses", "Smart"]
     weaknesses = ["Asthma", "Relies on glasses"]
     held = ["Glasses"]
     relations = {
-        "Name": 0,
-        "Name": 0,
-        "Name": 0
+        "Ralph": 1,
+        "Jack": 2,
+        "Roger": 3,
+        "Samneric": 4
     }
 
 class Ralph:
+    name = "Ralph"
     strengths = ["Athletic", "Good Charisma", "Leader"]
+    weaknesses = ["Bad with important decisions", "Fears a lot of things"]
     held = ["Bad with important decisions", "Fears a lot of things"]
     relations = {
-        "Name": 0,
-        "Name": 0,
-        "Name": 0
+        "Piggy": 1,
+        "Jack": 2,
+        "Roger": 3,
+        "Samneric": 4
     }
-
 class Jack:
+    name = "Jack"
     strengths = ["brave", "Leader of the hunters"]
     weaknesses = ["Fragile ego", "Overconfident"]
     held = []
     relations = {
-        "Name": 0,
-        "Name": 0,
-        "Name": 0
+        "Piggy": 1,
+        "Ralph": 2,
+        "Roger": 3,
+        "Samneric": 4
     }
 
 class Roger:
+    name = "Roger"
     strengths = ["Good with weapons"]
     weaknesses = ["Isolation"]
     held = []
     relations = {
-        "Name": 0,
-        "Name": 0,
-        "Name": 0
+        "Piggy": 1,
+        "Ralph": 2,
+        "Jack": 3,
+        "Samneric": 4
     }
 
 class Samneric:
+    name = "Samneric"
     strengths = ["Civilised", "Reasonable"]
     weaknesses = ["Easily intimidated"]
     held = []
     relations = {
-        "Name": 0,
-        "Name": 0,
-        "Name": 0
+        "Piggy": 1,
+        "Ralph": 2,
+        "Jack": 3,
+        "Roger": 4
     }
 
-def Choose(person, text="What would you like to do?", choices=[]):
+def Choice(person, text="What would you like to do?", choices=[]):
     print(text)
     for x in choices:
-        print(f"{choices.index(x)+1}. {x}")
+        print(f"{choices.index(x)+1}. {x}") #Lists the choices
     
-    choice = input("Number: ")
+    while True:
+        choice = input("Number: ")
+        try:
+            choice = int(choice)
+        except:
+            print("That is not a valid answer, please answer again with a number")
+        else:    
+            if choice <= len(choices) and choice > 0:
+                break
+            print("That is not a valid answer, please answer again with a number")
+
+    print("") #just adding space after line
     return choice
 
+def ShowPersonInfo(person):    
+    p = person
 
-Choose(Piggy, "What would you like to dooooo", ["Sleep", "Take a bath", "Eat", "Trade"])
+    print(f"Name: {p.name}")
+    print(f"Held: {p.held}")
+    print(f"Strengths: {p.strengths}")
+    print(f"Weaknesses: {p.weaknesses}")
+    print("Relations:")
+    for x in p.relations:
+        print(f"{x} = {p.relations[x]}") #printing the data in rows
 
-Choose(Piggy, choices=["sell", "Destroy", "Eat"])
 
+#CODE TESTING AREA
+
+#ShowPersonInfo(Ralph)
+
+#Choice(Piggy, "What would you like to do", ["Sleep", "Take a bath", "Eat", "Trade"])
+
+#Choice(Piggy, choices=["sell", "Destroy", "Eat"])
+
+
+
+#MAIN AREA FOR RUNNING CODE
+while True: #starting the main game loop
+    print("Weclome to, Let's try... diplomacy!")
+    print("This game is designed by Lila, Thomas, Thor, Viki and Zayan, and coded by Thor \n")
+    print("This game is based on the book Lord Of The Flies written by William Golding")
+
+
+
+    break #TEMPORARY SAFESTOP
